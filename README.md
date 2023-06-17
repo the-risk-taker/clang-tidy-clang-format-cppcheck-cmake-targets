3dbfb41d2f70103b27965a0713fc2685afa6e4b4
Mateusz Patyk <matpatyk@gmail.com>
2023-06-17 15:57:33 +0200

    Final version

---
+++ README.md
@@ -0,0 +1,17 @@
+# Clang-Format, Clang-Tidy, Cppcheck and CMake targets aka CMake C++ Starter Project
+
+[![CI pipeline](https://github.com/the-risk-taker/clang-tidy-clang-format-cppcheck-cmake-targets/actions/workflows/ci-pipeline.yml/badge.svg?branch=main)](https://github.com/the-risk-taker/clang-tidy-clang-format-cppcheck-cmake-targets/actions/workflows/ci-pipeline.yml)
+
+CMake C++ starter project with already provided Clang-Tidy, Clang-Format, Cppcheck targets and other helpful features.
+
+## Features
+
+![](https://img.shields.io/badge/Code_Formatter-Clang--Format-purple)
+![](https://img.shields.io/badge/Static_Analyzer-Clang--Tidy-blue)
+![](https://img.shields.io/badge/Static_Analyzer-Cppcheck-blue)
+![](https://img.shields.io/badge/CICD-GitHub_Actions-green)
+
+## TODO
+
+- [ ] sanitizers
+- [ ] testing framework
---
+++ cmake/clang-tidy.cmake
@@ -0,0 +1,39 @@
+find_program(CLANG_TIDY clang-tidy)
+
+if(NOT CLANG_TIDY)
+    message(STATUS "Clang-Tidy was not found, static-analysis targets are inaccessible")
+else()
+    add_custom_target(clang-tidy-check COMMAND ${CLANG_TIDY} ${ALL_SOURCES} -p=${CMAKE_CURRENT_BINARY_DIR} -use-color
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        USES_TERMINAL
+        COMMENT "Checking code with Clang-Tidy"
+    )
+
+    add_custom_target(clang-tidy-fix COMMAND ${CLANG_TIDY} ${ALL_SOURCES} -p=${CMAKE_CURRENT_BINARY_DIR} -use-color -fix-errors
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        USES_TERMINAL
+        COMMENT "Applying automatic fixes to code with Clang-Tidy"
+    )
+    
+    message(STATUS "Clang-Tidy was found, 'clang-tidy-check' and 'clang-tidy-fix' targets can be used")
+endif()
+
+find_program(RUN_CLANG_TIDY run-clang-tidy)
+
+if(NOT RUN_CLANG_TIDY)
+    message(STATUS "Run-Clang-Tidy was not found, static-analysis targets are inaccessible")
+else()
+    add_custom_target(run-clang-tidy-check COMMAND ${RUN_CLANG_TIDY} -p=${CMAKE_CURRENT_BINARY_DIR} -format
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        USES_TERMINAL
+        COMMENT "Checking code with run-clang-tidy (parallel)"
+    )
+
+    add_custom_target(run-clang-tidy-fix COMMAND ${RUN_CLANG_TIDY} -p=${CMAKE_CURRENT_BINARY_DIR} -format -fix
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        USES_TERMINAL
+        COMMENT "Applying automatic fixes to code with run-clang-tidy (parallel)"
+    )
+
+    message(STATUS "Run-Clang-Tidy was found, 'run-clang-tidy-check' and 'run-clang-tidy-fix' targets can be used")
+endif()
