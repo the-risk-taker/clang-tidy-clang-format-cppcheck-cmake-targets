64412d32aec865d88acd2cffa7cee7f1a454eb1c
Mateusz Patyk <matpatyk@gmail.com>
2023-10-23 00:19:41 +0200

    Usage fixes
    Collect Sources: divided all sources to headers and sources
    
    Clang-Tidy: removed '-format*' from check targets
    
    Cppcheck: removed passing sources when using with '--project'

--- cmake/collect-sources.cmake
+++ cmake/collect-sources.cmake
@@ -1,4 +1,6 @@
 file(GLOB_RECURSE ALL_SOURCES *.h *.c *.hpp *.cpp)
 list(FILTER ALL_SOURCES EXCLUDE REGEX ${CMAKE_BINARY_DIR})
+file(GLOB_RECURSE ALL_HEADERS *.h *.c *.hpp *.cpp)
+list(FILTER ALL_HEADERS EXCLUDE REGEX ${CMAKE_BINARY_DIR})
 
 message(STATUS "All sources collected")
